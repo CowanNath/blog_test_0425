f7c7b255ed17b5254fbcb81a37cbb8d78c747294
Cowan Nath <737620843@qq.com>
2019-04-27 01:00:44 +0800

    0426 code

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 账号：wang
 密码：test123456
+
+python version：3.7.1
+django version：2.1
+
+ubuntu下virtualenv环境启动 source 路径/bin/activate 
---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Blog, BlogType
+
+# Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "author","blog_type", "created_time")
+
+@admin.register(BlogType)
+class BlogTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "type_name")
